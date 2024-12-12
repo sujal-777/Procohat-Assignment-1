0a0dc7b3e137619d4cf3c99dd635cb18d781e81f
Sujal <palsujal707@gmail.com>
2024-12-13 03:27:20 +0530

    added

--- frontend/src/services/employeeService.jsx
+++ frontend/src/services/employeeService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://hospital-backend-alpha.vercel.app/api/employees';
+const API_URL = `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api/employees`;
 
 export const fetchEmployees = async () => {
     try {
